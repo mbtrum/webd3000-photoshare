037f8de43fc5c5e47bd96926256bb95f0ad4f1bd
Mike Trumbull <michael.trumbull@nscc.ca>
2025-01-14 14:25:23 -0400

    Add project files.

---
+++ PhotoShare/Models/Photo.cs
@@ -0,0 +1,21 @@
+﻿namespace PhotoShare.Models
+{
+    public class Photo
+    {
+        // Primary key
+        public int PhotoId { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+
+        public string ImageFilename { get; set; } = string.Empty;
+
+        public bool IsPublic { get; set; } = false;
+
+        // Navigation property
+        public List<Photo>? Tags { get; set; } // nullable!!
+    }
+}
