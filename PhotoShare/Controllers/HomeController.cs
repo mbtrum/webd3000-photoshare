23dee66ca3d62eff3124fe63dd8fc22487bbd565
Mike Trumbull <michael.trumbull@nscc.ca>
2025-01-20 16:17:15 -0400

    Scaffolding and migration.

--- PhotoShare/Controllers/HomeController.cs
+++ PhotoShare/Controllers/HomeController.cs
@@ -15,36 +15,32 @@ namespace PhotoShare.Controllers
         {
             // To-do: Entity Framework - fetch all photos
 
-
             // Create a list of photos
             List<Photo> photos = new List<Photo>();
 
             // Create photo object
             Photo photo1 = new Photo () { 
-                PhotoId = 1, 
-                Title = "My cat", 
+                PhotoId = 1,                 
                 Description = "This is my cat", 
                 ImageFilename = "cat.jpg",
-                IsPublic = true
+                IsVisible = true
             };
 
             // Create photo object
             Photo photo2 = new Photo();
 
             photo2.PhotoId = 2;
-            photo2.Title = "Dog";
             photo2.Description = "This is my dog";
             photo2.ImageFilename = "dog.jpg";
-            photo2.IsPublic = true;
+            photo2.IsVisible = true;
 
             // Create photo object
             Photo photo3 = new Photo();
 
             photo3.PhotoId = 3;
-            photo3.Title = "Hamster";
             photo3.Description = "This is my hamster";
             photo3.ImageFilename = "hamster.jpg";
-            photo3.IsPublic = true;
+            photo3.IsVisible = true;
 
             // Add them to the list
             photos.Add(photo1);
@@ -54,18 +50,17 @@ namespace PhotoShare.Controllers
             return View(photos);
         }
 
-        // Display a photo by id - ../Home/DisplayPhoto/
-        public IActionResult DisplayPhoto(int id)
+        // Display a photo by id - ../Home/PhotoDetails/
+        public IActionResult PhotoDetails(int id)
         {
             // To-do: Entity Framework - fetch the photo by id
 
             Photo photo = new Photo()
             {
                 PhotoId = id,
-                Title = "Bird",
                 Description = "This is my bird",
                 ImageFilename = "bird.jpg",
-                IsPublic = true
+                IsVisible = true
             };
 
             return View(photo);
